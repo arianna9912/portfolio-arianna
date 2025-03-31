38cc59b3d5cff2c4504ed67a85efef1c35696687
Arianna <apr991212@gmail.com>
2025-03-31 17:01:00 -0400

    Links and metadata

--- src/components/MobileNav/MobileNav.jsx
+++ src/components/MobileNav/MobileNav.jsx
@@ -9,26 +9,27 @@ const MobileNav = ({ isOpen, toggleMenu }) => {
         <>
             <div className={`mobile-menu ${isOpen ? "active" : ""}`} onClick={toggleMenu}>
                 <div className='mobile-menu-container'>
-                    <h1 className="text-white ml-0">Arianna<strong className="text-purple-600">9912</strong></h1>
+                    <h1 className="text-white ml-0">  <a href="https://github.com/arianna9912" target="_blank" rel="noopener noreferrer">
+                        Arianna<strong className="text-purple-600">9912</strong> </a></h1>
                     <ul>
                         <li>
-                        <RouterLink to="/"    className="menu-item" onClick={() => setTimeout(() => { window.scrollTo(0, document.getElementById('home').offsetTop); }, 0)}>Home</RouterLink>
-            
-                             </li>
-                        <li>
-                        <RouterLink to="/about"    className="menu-item" onClick={() => setTimeout(() => { window.scrollTo(0, document.getElementById('about').offsetTop); }, 0)}>About Me</RouterLink>
-            
-                                 </li>
-                        <li>
-                        <RouterLink to="/skills"    className="menu-item" onClick={() => setTimeout(() => { window.scrollTo(0, document.getElementById('skills').offsetTop); }, 0)}>Skills</RouterLink>
+                            <RouterLink to="/" className="menu-item" onClick={() => setTimeout(() => { window.scrollTo(0, document.getElementById('home').offsetTop); }, 0)}>Home</RouterLink>
+
                         </li>
                         <li>
-                        <RouterLink to="/projects"    className="menu-item" onClick={() => setTimeout(() => { window.scrollTo(0, document.getElementById('projects').offsetTop); }, 0)}>Projects</RouterLink>
-               
+                            <RouterLink to="/about" className="menu-item" onClick={() => setTimeout(() => { window.scrollTo(0, document.getElementById('about').offsetTop); }, 0)}>About Me</RouterLink>
+
                         </li>
                         <li>
-                        <RouterLink to="/contact"    className="menu-item" onClick={() => setTimeout(() => { window.scrollTo(0, document.getElementById('contact').offsetTop); }, 0)}>Contact Me</RouterLink>
-               
+                            <RouterLink to="/skills" className="menu-item" onClick={() => setTimeout(() => { window.scrollTo(0, document.getElementById('skills').offsetTop); }, 0)}>Skills</RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink to="/projects" className="menu-item" onClick={() => setTimeout(() => { window.scrollTo(0, document.getElementById('projects').offsetTop); }, 0)}>Projects</RouterLink>
+
+                        </li>
+                        <li>
+                            <RouterLink to="/contact" className="menu-item" onClick={() => setTimeout(() => { window.scrollTo(0, document.getElementById('contact').offsetTop); }, 0)}>Contact Me</RouterLink>
+
                         </li>
                     </ul>
                 </div>
