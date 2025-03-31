6000e5237f36d740687c833636ac914349332ce3
Arianna <apr991212@gmail.com>
2025-03-31 13:47:38 -0400

    Main routes

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio-arianna">
       <App />
     </BrowserRouter>
   </React.StrictMode>
