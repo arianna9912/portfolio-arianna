8c457bda76fec49eed92e122e99e75204f3caa83
Arianna <apr991212@gmail.com>
2025-03-31 14:02:58 -0400

    Hero Section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react"
 import "./Hero.css"
-import meImage from '../../assets/ari-img.jpg';
-import reactIcon from '../../assets/react.svg';
-import vueIcon from '../../assets/logo.png'
-import javaIcon from '../../assets/javascript-logo.png'
-import tailIcon from '../../assets/tailwind.png'
+import meImage from '../assets/ari-img.jpg';
+import reactIcon from '../assets/react.svg';
+import vueIcon from '../assets/logo.png'
+import javaIcon from '../assets/javascript-logo.png'
+import tailIcon from '../assets/tailwind.png'
 
 
 const Hero = () => {
@@ -48,7 +48,7 @@ const Hero = () => {
                     <p className="text-white text-xl">Passionate Frontend Developer. I excel at creating elegant digital experiences and I am focused on developing user-friendly and intuitive interfaces with a strong background in responsive design.</p>
                     <div className="flex flex-wrap items-center  mt-4">
 
-                        <a href="/CV.pdf"
+                        <a href="/portfolio-arianna/CV.pdf"
                             download="Arianna-CV.pdf" className="border border-purple-600 text-purple-600 bg-transparent hover:bg-purple-700 hover:text-white py-2 px-4 rounded-full flex items-center">
                             <span className="text-lg">Download CV</span> <svg xmlns="http://www.w3.org/2000/svg" className="animate-bounce transition-colors duration-300" width="24" height="24" viewBox="0 0 24 24"><path className="fill-current" fill="#8a27ce" d="M18.22 20.75H5.78A2.64 2.64 0 0 1 3.25 18v-3a.75.75 0 0 1 1.5 0v3a1.16 1.16 0 0 0 1 1.25h12.47a1.16 1.16 0 0 0 1-1.25v-3a.75.75 0 0 1 1.5 0v3a2.64 2.64 0 0 1-2.5 2.75" /><path className="fill-current" fill="#8a27ce" d="M12 15.75a.74.74 0 0 1-.53-.22l-4-4a.75.75 0 0 1 1.06-1.06L12 13.94l3.47-3.47a.75.75 0 0 1 1.06 1.06l-4 4a.74.74 0 0 1-.53.22" /><path className="fill-current" fill="#8a27ce" d="M12 15.75a.76.76 0 0 1-.75-.75V4a.75.75 0 0 1 1.5 0v11a.76.76 0 0 1-.75.75" /></svg>  </a>
 
